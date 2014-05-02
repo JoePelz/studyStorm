221e785d430e4020688bd1ef3d890201f1455f26
Joe Pelz <joe.pelz@gmail.com>
2014-05-01 19:55:58 -0700

    Added initial files

---
+++ details.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+</head>
+
+<body>
+
+<?php
+	echo "<h1>".$_GET['name']."'s study session!</h1>";
+	echo "<p>subject: </p>";
+	echo "<p>time: </p>";
+	echo "<p>location: </p>";
+	echo "<p>description: </p>";
+?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<h1>Study Storm</h1>
+	<a href="addSessionForm.html">Add session</a>
+	<hr />
+	<ul>
+		<li><a href="details.php?name=Carter">Carter</a></li>
+		<li><a href="details.php?name=Arnold">Arnold</a></li>
+		<li><a href="details.php?name=Samuel">Samuel</a></li>
+	</ul>
+</body>
+</html>
