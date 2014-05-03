0f56a638fea1c1e00ff220b0df4dbfaff8967da9
Joe Pelz <joe.pelz@gmail.com>
2014-05-03 14:27:23 -0700

    added main page and session details page

--- details.php
+++ details.php
@@ -1,17 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
+<meta charset="utf-8">
+<title>Details</title>
 </head>
 
 <body>
 
 <?php
-	echo "<h1>".$_GET['name']."'s study session!</h1>";
-	echo "<p>subject: </p>";
-	echo "<p>time: </p>";
-	echo "<p>location: </p>";
-	echo "<p>description: </p>";
+require_once('./php/config.php');
+
+$con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+if (!$con) {
+	die('Failed to connect to server: ' . mysql_error());
+}
+
+$db = mysql_select_db(DB_DATABASE);
+if(!$db) {
+	die("Unable to select database");
+}
+//data available: studName, sessionId, courseName, details, startTime, endTime, location, studId, isActive
+$qry = "SELECT u.studName, s.* FROM sessions s, students u WHERE u.studId=s.studId AND s.sessionId=".$_GET['sessionId'];
+$result = mysql_query($qry);
+
+
+if ($result) {
+	while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+		echo "<h1>".$row['studName']."'s Study Session</h1>\n";
+		echo "<ul>\n";
+		echo "<li>Subject: ".$row['courseName']."</li>\n";
+		echo "<li>Time: ".$row['startTime']." to ".$row['endTime']."</li>\n";
+		echo "<li>Location: ".$row['location']."</li>\n";
+		echo "<li>Details: ".$row['details']."</li>\n";
+		echo "</ul>\n";
+	}
+} else {
+	die("Query failed");
+}
+
+mysql_close($con);
 ?>
 
 </body>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+session_start();
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
@@ -7,6 +9,7 @@
 
 <body>
 <h1>Study Storm</h1>
+
 <?php
 if (isset($_SESSION['email'])) {
 echo 'Welcome, ' . $_SESSION['studName'] . '!<hr>';
@@ -17,12 +20,42 @@ echo 'Welcome, ' . $_SESSION['studName'] . '!<hr>';
 <a href="signInForm.html">Sign in</a>
 <a href="registerForm.html">Register</a>
 <a href="addSessionForm.html">Add Session</a>
-<a href="addSessionForm.html">Add session</a>
 <hr />
 	<ul>
-		<li><a href="details.php?name=Carter">Carter</a></li>
-		<li><a href="details.php?name=Arnold">Arnold</a></li>
-		<li><a href="details.php?name=Samuel">Samuel</a></li>
+
+<?php
+require_once('./php/config.php');
+
+$con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+if (!$con) {
+	die('Failed to connect to server: ' . mysql_error());
+}
+
+$db = mysql_select_db(DB_DATABASE);
+if(!$db) {
+	die("Unable to select database");
+}
+
+//data available: studName, sessionId, courseName, details, startTime, endTime, location, studId, isActive
+$qry = "SELECT u.studName, s.startTime, s.sessionId FROM sessions s, students u WHERE u.studId=s.studId AND s.isActive=1";
+$result = mysql_query($qry);
+
+
+if ($result) {
+	while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+		echo "\t\t<li>";
+		echo "<a href='details.php?sessionId=".$row['sessionId']."'>";
+		echo $row['studName']."</a>\t";
+		echo $row['startTime']."\t";
+		echo "</li>\n";
+	}
+} else {
+	die("Query failed");
+}
+
+mysql_close($con);
+?>
+
 	</ul>
 </body>
 </html>
