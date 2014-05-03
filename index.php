7f7cb81a9eaf5c0f5be171fabce40273537ea242
Joe Pelz <joe.pelz@gmail.com>
2014-05-03 14:37:09 -0700

    cleaned up mysql queries slightly

--- index.php
+++ index.php
@@ -26,15 +26,11 @@ echo 'Welcome, ' . $_SESSION['studName'] . '!<hr>';
 <?php
 require_once('./php/config.php');
 
-$con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-if (!$con) {
-	die('Failed to connect to server: ' . mysql_error());
-}
+$con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD)
+	or die('Failed to connect to server: ' . mysql_error());
 
-$db = mysql_select_db(DB_DATABASE);
-if(!$db) {
-	die("Unable to select database");
-}
+$db = mysql_select_db(DB_DATABASE) or die("Unable to select database");
+
 
 //data available: studName, sessionId, courseName, details, startTime, endTime, location, studId, isActive
 $qry = "SELECT u.studName, s.startTime, s.sessionId FROM sessions s, students u WHERE u.studId=s.studId"; #AND s.isActive=1";
