9f3c96e5e5dae3be85c55e8ba7c7f0dc0359fe63
Joe Pelz <joe.pelz@gmail.com>
2014-05-12 14:02:54 -0700

    TimeStamp:  addSession now uses it.

--- php/addSession.php
+++ php/addSession.php
@@ -26,22 +26,35 @@ $location = mysql_real_escape_string($_POST['location']);
 $details = mysql_real_escape_string($_POST['details']);
 $studId = $_SESSION['studId'];
 
+//convert start and end times into timestamps
+date_default_timezone_set("America/Vancouver");
+//get current date and time
+$start = new DateTime();
+//replace hour/min/second
+$startHour = substr($startTime, 0, 2);
+$startMinute = substr($startTime, 3, 2);
+$start->setTime($startHour, $startMinute);
+//turn date + time into timestamp
+$startTS = $start->format('Y-m-d H:i:s');
 
-echo $courseName . "<BR>";
-echo $startTime . "<BR>";
-echo $endTime . "<BR>";
-echo $location . "<BR>";
-echo $details . "<BR>";
+//get current date and time
+$end = new DateTime();
+//replace hour/min/second
+$endHour = substr($endTime, 0, 2);
+$endMinute = substr($endTime, 3, 2);
+$end->setTime($endHour, $endMinute);
+//turn date + time into timestamp
+$endTS = $end->format('Y-m-d H:i:s');
 
 
-$myrequ = "INSERT INTO sessions (courseName, startTime, endTime, location, details, studID, isActive) VALUES ('$courseName', '$startTime', '$endTime', '$location', '$details', '$studId', 1)";
+$myrequ = "INSERT INTO sessions (courseName, startTime, endTime, startTime2, endTime2, location, details, studID, isActive) VALUES ('$courseName', '$startTime', '$endTime', '$startTS', '$endTS', '$location', '$details', '$studId', 1)";
 $result = mysql_query($myrequ);
 
 if ($result) {
 	echo 'Success!';
 	exit();
 } else {
-		echo 'ERROR, did not add to database!' . mysql_error();
+	echo 'ERROR, did not add to database!' . mysql_error();
 }
 
 mysql_close($con);
