329ead0357282a972221dca976c52e82adbc2dd4
Joe Pelz <joe.pelz@gmail.com>
2014-05-22 09:43:01 -0700

    updated logout

--- php/logout.php
+++ php/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 session_destroy();
-setcookie("username", $_SESSION['studName'], time() - 1);
+setcookie("email", " ", time() - 1, '/');
 echo "Success!";
 //////////////////////////////////////////
 //
